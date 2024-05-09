770493aa4751eb1e94c92455b786ffdcd8d54dd6
visheshdubey <visheshdubey2016@gmail.com>
2024-05-09 23:24:56 +0530

    Added sidebar ui to the job page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Checkbox } from "@/components/ui/checkbox";
 
 import { Slider } from "./_components/slider";
 import { Button } from "@/components/ui/button";
@@ -16,6 +17,7 @@ import {
   icons,
 } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type Props = {};
 
@@ -25,7 +27,7 @@ const Page = (props: Props) => {
   ]);
   return (
     <div className="relative flex flex-col ">
-      <nav className="flex items-center h-16 w-full bg-background sticky border-b top-0">
+      <nav className="flex z-50 shadow items-center h-16 w-full bg-background sticky border-b top-0">
         <div className="flex container px-4 justify-between">
           <span className="text-xl font-medium">Openhiring</span>
           <div className="flex gap-4">
@@ -42,34 +44,162 @@ const Page = (props: Props) => {
           className="flex justify-center flex-col shrink-0 w-1/4 sticky top-16"
           style={{ height: "calc(100vh - 4rem)" }}
         >
-          <div className="bg-card border rounded-md p-4">
-            <span className="font-semibold">All filters</span>
-            <div className="flex flex-col">
-              <span className="text-muted-foreground">Salary</span>
-              <Slider
-                defaultValue={[0, 50]}
-                min={0}
-                max={100}
-                step={1}
+          <ScrollArea
+            className="rounded-md my-8 bg-card border shadow-sm"
+            style={{ height: "calc(100vh - 4rem)" }}
+          >
+            <div className="flex flex-col  mb-4  h-full rounded-md px-4 pt-4 gap-8">
+              <div className="flex justify-between items-center">
+                <span className="font-semibold">All filters</span>
+                <span className="text-muted-foreground text-sm">Reset</span>
+              </div>
 
-                //  value={sliderValueWhileChange}
-                //  onValueChange={(x) => setSliderValueWhileChange(x)}
-                //  onValueCommit={(x) => dispatch(updateSalaryFilter(x))}
-              />
+              <div className="flex flex-col gap-4">
+                <span className="text-muted-foreground font-medium">
+                  Salary
+                </span>
+                <Slider
+                  defaultValue={[0, 50]}
+                  min={0}
+                  max={100}
+                  step={1}
+
+                  //  value={sliderValueWhileChange}
+                  //  onValueChange={(x) => setSliderValueWhileChange(x)}
+                  //  onValueCommit={(x) => dispatch(updateSalaryFilter(x))}
+                />
+                <div className="flex text-muted-foreground text-sm justify-between">
+                  <span>$500</span>
+                  <span>$500</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <span className="text-muted-foreground font-medium">
+                  Department
+                </span>
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      Fullstack
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      Frontent
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      Backend
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <span className="text-muted-foreground font-medium">
+                  Work type
+                </span>
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      Hybrid
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      On-site
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      Remote
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <span className="text-muted-foreground font-medium">
+                  Experience
+                </span>
+                <div className="flex flex-col gap-4">
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      0 - 1 Years
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      1 - 2 Years
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      2 - 3 Years
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Checkbox id="terms1" />
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+                    >
+                      3+ Years
+                    </label>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </ScrollArea>
         </div>
-        <div className="flex flex-col gap-4  h-[8080px] m-8">
-          <div className="w-full bg-primary h-48 rounded"></div>
+        <div className="flex flex-col gap-4  m-8">
+          {/* <div className="w-full bg-primary h-48 rounded"></div> */}
           <div className="flex items-center">
-            <span className="grow">Filters</span>
+            <span className="grow text-sm text-muted-foreground">
+              28323 Results
+            </span>
             <Button>Create +</Button>
           </div>
 
           {Array.from({ length: 25 }).map((item, index) => (
             <div
               key={index}
-              className="flex flex-col w-full border hover:bg-card  p-4 gap-4 transition-all duration-200 ease-in-out rounded-lg"
+              className="flex hover:cursor-pointer hover:border-primary flex-col w-full border hover:bg-card  p-4 gap-4 transition-all duration-300 ease-in-out rounded-lg"
             >
               <div className="flex items-center">
                 <div className="flex items-center grow gap-4">
